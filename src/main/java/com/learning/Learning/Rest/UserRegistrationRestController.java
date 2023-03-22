6ab68db94166ddbaf91ea9802dbd7a30fd7a6648
RollyLif <relifungula@gmail.com>
2023-03-22 22:11:50 +0200

    cleaning everything for rewrite for good a get method

--- src/main/java/com/learning/Learning/Rest/UserRegistrationRestController.java
+++ src/main/java/com/learning/Learning/Rest/UserRegistrationRestController.java
@@ -1,11 +1,18 @@
 package com.learning.Learning.Rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learning.Learning.dto.UserDTO;
 import com.learning.Learning.repository.UserJpaRepository;
 
 @RestController
@@ -17,5 +24,16 @@ public class UserRegistrationRestController {
 	@Autowired
 	public void setUserJpaRepository(UserJpaRepository userJpaRepository) {
 		this.userJpaRepository = userJpaRepository;
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<UserDTO>> listAllUsers(){
+		List<UserDTO> users = new ArrayList<UserDTO>();
+		try {
+			users = UserJpaRepository.findAll();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<List<UserDTO>>(users, HttpStatus.OK);
 	}
 }
