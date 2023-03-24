37c5fa0fc16acb44c68f204f641b396bb2f1e8ca
RollyLif <relifungula@gmail.com>
2023-03-24 20:46:55 +0200

    adding post endpoint

--- src/main/java/com/learning/Learning/Rest/UserRegistrationRestController.java
+++ src/main/java/com/learning/Learning/Rest/UserRegistrationRestController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,11 @@ public class UserRegistrationRestController {
 	public ResponseEntity<List<UserDTO>> listAllUsers() {
 		List<UserDTO> users = userJpaRepository.findAll();
 		return new ResponseEntity<List<UserDTO>>(users,HttpStatus.OK );
+	}
+	
+	@PostMapping("/")
+	public ResponseEntity<UserDTO> createUser(@RequestBody final UserDTO user){
+		userJpaRepository.save(user);
+		return new ResponseEntity<UserDTO>(user, HttpStatus.CREATED);
 	}
 }
