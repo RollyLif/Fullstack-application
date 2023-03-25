d681a7aae3d467d48941c5b948aad734ce4aa216
RollyLif <relifungula@gmail.com>
2023-03-25 22:08:43 +0200

    adding endpoint for delete user by id

--- src/main/java/com/learning/Learning/Rest/UserRegistrationRestController.java
+++ src/main/java/com/learning/Learning/Rest/UserRegistrationRestController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,8 +45,28 @@ public class UserRegistrationRestController {
 	}
 	
 	@GetMapping("/{name}")
-	public ResponseEntity<UserDTO> getUserById(@PathVariable("name") String name){
+	public ResponseEntity<UserDTO> getUserById(@PathVariable("name") final String name){
 		UserDTO user = userJpaRepository.findByName(name);
 		return new ResponseEntity<UserDTO>(user, HttpStatus.OK);
 	}
+	/*
+	@PutMapping(value="/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserDTO> updateUser(@PathVariable("id") final Long id, @RequestBody UserDTO user){
+		UserDTO currentuser = userJpaRepository.findById(id);
+		
+		currentuser.setName(user.getName());
+		currentuser.setAddress(user.getAddress());
+		currentuser.setEmail(user.getEmail());
+		
+		userJpaRepository.saveAndFlush(currentuser);
+		
+		return new ResponseEntity<UserDTO>(currentuser, HttpStatus.OK);
+	}
+	*/
+	@DeleteMapping("/{id}")
+	public ResponseEntity<UserDTO> deleteUser(@PathVariable("id") final Long id){
+		userJpaRepository.deleteById(id);
+		return new ResponseEntity<UserDTO>(HttpStatus.NO_CONTENT);
+	}
+	
 }
