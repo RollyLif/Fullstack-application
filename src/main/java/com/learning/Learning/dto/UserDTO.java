9e21a889ae884bd66076bda0dbb5f254ca73a965
RollyLif <relifungula@gmail.com>
2023-03-30 20:16:31 +0200

    adding hibernate and glassfish as dependecies in the project

--- src/main/java/com/learning/Learning/dto/UserDTO.java
+++ src/main/java/com/learning/Learning/dto/UserDTO.java
@@ -1,5 +1,9 @@
 package com.learning.Learning.dto;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,12 +22,19 @@ public class UserDTO {
 	@Column(name= "USER_ID")
 	private Long id;
 	
+	@NotBlank
+	@Length(max=50)
 	@Column(name="NAME")
 	private String name;
 	
+	@NotBlank
+	@Length(max=150)
 	@Column(name="ADRESS")
 	private String address;
 	
+	@Email
+	@NotBlank
+	@Length(max =80)
 	@Column(name="EMAIL")
 	private String email;
 	
