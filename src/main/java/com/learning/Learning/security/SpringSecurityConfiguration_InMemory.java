b6facc3ae53983c10b6c8fd3f192e90f52fe25d4
RollyLif <relifungula@gmail.com>
2023-04-17 19:49:19 +0200

    creating for spring security

--- src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_InMemory.java
@@ -1,28 +1,31 @@
 package com.learning.Learning.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-@Order(SecurityProperties.BASIC_AUTH_ORDER)
-public class SpringSecurityConfiguration_InMemory extends WebSecurityConfiguration {
+@EnableWebSecurity
+public class SpringSecurityConfiguration_InMemory{
 
-	@Autowired
-	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
-		auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN", "USER");
-	}
-
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeHttpRequests().requestMatchers(HttpMethod.GET, "/api/user/").hasRole("USER")
-				.requestMatchers(HttpMethod.POST, "/api/user/").hasRole("USER")
-				.requestMatchers(HttpMethod.PUT, "/api/user/**").hasRole("USER")
-				.requestMatchers(HttpMethod.DELETE, "/api/user/**").hasRole("ADMIN").and().csrf().disable();
+	@Bean
+	public InMemoryUserDetailsManager userDetailsManager() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		
+		UserDetails admin = User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("password")
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user,admin);
 	}
 }
