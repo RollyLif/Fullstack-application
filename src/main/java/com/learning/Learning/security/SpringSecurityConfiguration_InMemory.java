93710334ac78f3c8b725edfe316d70d592006048
RollyLif <relifungula@gmail.com>
2023-04-17 21:33:29 +0200

    configuration of spring security without WebSecurityConfigurerAdapter

--- src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_InMemory.java
@@ -2,11 +2,14 @@ package com.learning.Learning.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -23,9 +26,22 @@ public class SpringSecurityConfiguration_InMemory{
 		UserDetails admin = User.withDefaultPasswordEncoder()
 				.username("admin")
 				.password("password")
-				.roles("ADMIN")
+				.roles("ADMIN","USER")
 				.build();
 		
 		return new InMemoryUserDetailsManager(user,admin);
+	}
+	
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth ->{
+					auth.requestMatchers(HttpMethod.GET, "/api/user/").hasRole("USER");
+					auth.requestMatchers(HttpMethod.POST, "/api/user/").hasRole("USER");
+					auth.requestMatchers(HttpMethod.PUT, "/api/user/**").hasRole("USER");
+					auth.requestMatchers(HttpMethod.DELETE, "/api/user/**").hasRole("ADMIN");
+				})
+				.httpBasic(Customizer.withDefaults())
+				.build();
 	}
 }
