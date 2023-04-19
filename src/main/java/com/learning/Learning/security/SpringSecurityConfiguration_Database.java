ec5d16f6bdad7928a1a82e808f03505e5d467d4a
RollyLif <relifungula@gmail.com>
2023-04-19 23:29:21 +0200

    removing every from configure

---
+++ src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_Database.java
@@ -0,0 +1,16 @@
+package com.learning.Learning.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration_Database {
+	
+	SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		return null
+	}
+}
