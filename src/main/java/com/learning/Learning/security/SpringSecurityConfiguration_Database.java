154d1da2f25524227c1c2035914f8e97a1b9b21b
RollyLif <relifungula@gmail.com>
2023-04-20 21:58:31 +0200

    configure a session

--- src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_Database.java
+++ src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_Database.java
@@ -1,6 +1,7 @@
 package com.learning.Learning.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,15 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurityConfiguration_Database {
 	
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		return null
+		return http
+				.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/api/user/**")
+						.permitAll()
+						.anyRequest().authenticated()
+						)
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.httpBasic(Customizer.withDefaults())
+				.build();
 	}
 }
