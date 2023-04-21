a52666a0979de94c8d9a5f75994b47da14cb6db4
RollyLif <relifungula@gmail.com>
2023-04-21 12:07:03 +0200

    implenting basic authentication manager

--- src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_Database.java
+++ src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_Database.java
@@ -1,7 +1,10 @@
 package com.learning.Learning.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,7 +14,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SpringSecurityConfiguration_Database {
 	
-	SecurityFilterChain configure(HttpSecurity http) throws Exception {
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+	
+	
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		return http
 				.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests(auth -> auth
