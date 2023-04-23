cc61d5fd23c3843ed5dcc2042fffc18c43d23f6f
RollyLif <relifungula@gmail.com>
2023-04-23 21:49:55 +0200

    comment all about userInfo

--- src/main/java/com/learning/Learning/Service/UserInfoDetailsService.java
+++ src/main/java/com/learning/Learning/Service/UserInfoDetailsService.java
@@ -23,17 +23,18 @@ public class UserInfoDetailsService implements UserDetailsService{
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+/*		
 		UserInfo user = userInfoJpaRepository.findByUsername(username);
 		if (user == null) {
 			throw new UsernameNotFoundException(
 					"Opps! user not found with user-name: "+ username);
 		}
-		
-		return new org.springframework.security.core.userdetails.User(
-				user.getUsername(), user.getPassword(), getAuthorities(user));
+	*/	
+		return null;
+				/* new org.springframework.security.core.userdetails.User(
+				user.getUsername(), user.getPassword(), getAuthorities(user)); */
 	}
-	
+
 	private Collection<GrantedAuthority> getAuthorities(UserInfo user){
 		List<GrantedAuthority> authorities = new ArrayList<>();
 		authorities = AuthorityUtils.createAuthorityList(user.getRole());
