c55bfeb1ff88e7b6b6b54025004cbba491edb407
RollyLif <relifungula@gmail.com>
2023-05-10 13:51:21 +0200

    updating app.js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,7 +2,10 @@ let app = angular.module('userregistrationsystem', ['ngRoute', 'ngResource']);
 
 app.config(function($routeProvider) {
         $routeProvider
-                .when('/list-all-users', {
+				.when('/',{
+					templateUrl : '/template/home.html',
+					controller : 'homeController'
+				}).when('/list-all-users', {
                         templateUrl : '/template/listuser.html',
                         controller : 'listUserController'
                 }).when('/register-new-user',{
@@ -11,12 +14,17 @@ app.config(function($routeProvider) {
                 }).when('/update-user/:id',{
                         templateUrl : '/template/userupdation.html' ,
                         controller : 'usersDetailsController'
-                }).otherwise({
-                        redirectTo : '/home',
-                        templateUrl : '/template/home.html',
+                }).when('/login',{
+						templateUrl : '/login/login.html',
+						controller : 'loginController'
+				}).when('/logout',{
+						templateUrl :'/login/login.html',
+						controller :'logoutController'
+				}).otherwise({
+                        redirectTo : '/login'
                 });
 });
 
 app.config(['$httpProvider', function($httpProvider) {
-  		$httpProvider.interceptors.push('AuthInterceptor');
+  		$httpProvider.defaults.headers.common["X-Requested-With"]='XMLHttpRequest';
 }]);
