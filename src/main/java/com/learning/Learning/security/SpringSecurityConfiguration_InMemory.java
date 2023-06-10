fe8964ef94fc740128725941eb8ae4a032fba0cb
RollyLif <relifungula@gmail.com>
2023-06-10 22:03:13 +0200

    removing an unused import

--- src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_InMemory.java
+++ src/main/java/com/learning/Learning/security/SpringSecurityConfiguration_InMemory.java
@@ -2,8 +2,6 @@ package com.learning.Learning.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -36,19 +34,15 @@ public class SpringSecurityConfiguration_InMemory{
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		return http
-			.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-			.authorizeHttpRequests((auth)-> {
-					auth.requestMatchers(HttpMethod.GET, "/api/user/").hasRole("user");
-					auth.requestMatchers(HttpMethod.POST, "/api/user/").hasRole("user");
-					auth.requestMatchers(HttpMethod.PUT, "/api/user/**").hasRole("user");
-					auth.requestMatchers(HttpMethod.DELETE, "/api/user/**").hasRole("admin");
-			})
+			.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))			
 			.authorizeHttpRequests()
 			.requestMatchers("/login/login.html","/template/home.html","/").permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
-			.httpBasic(Customizer.withDefaults())
+			.httpBasic()
+			.realmName("User Registration System")
+			.and()
 			.build();
 	}
 }
